4a5b3af3f3543bcd72e391f341939abb9cd5edfe
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-14 15:48:33 +0800

    exercise 9
    using for loop to add  smaller int into the
    finalSum and increment it by 1 every loop until
    you reach the bigger int. if function to check for
    arguments less than 0 (negative numbers) or
    non-integers and return 'ERROR' if true.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  let finalSum = 0
+  if (
+  int1 < 0 || 
+  int2 < 0 || 
+  !Number.isInteger(int1) || 
+  !Number.isInteger(int2)
+  ) {
+    return 'ERROR'
+  }
+  else if(int1 > int2) {
+    for (i = int2; i <= int1; i++) {
+      finalSum += i;
+    }
+  }
+  else {
+    for (i = int1; i <= int2; i++) {
+      finalSum += i;
+    }
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
