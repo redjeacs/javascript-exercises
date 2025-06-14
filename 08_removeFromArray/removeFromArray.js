aa416a48142263ad9d1b75053c5d48dd90aab35d
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-14 15:20:13 +0800

    exercise 8
    use the .forEach array method to apply chagnes to
    every item in the array. (...) spread method
    allows for multiple arguments in the function.
    for each item check if it is one of the rm.
    If not push it into the newArray.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rm) {
+  newArray = []
+  array.forEach(item => {
+    if (!rm.includes(item)) {
+      newArray.push(item);
+    }
+  })
+  return newArray;
 };
 
 // Do not edit below this line
