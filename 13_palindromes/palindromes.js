1a45d16499e5228eeaa4f19889900cb0b0a77290
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-18 15:59:55 +0800

    palindromes exercise
    convert original string into its reversed version
    with no punctuations, white space, and
    capitalizations and compare the two to see if it's
    a palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanedString = string
+    .replaceAll(/\p{P}/gu, '')
+    .replaceAll(' ', '')
+    .toLowerCase();
+  const reversedString = cleanedString
+    .split('')
+    .toReversed()
+    .join('');
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
