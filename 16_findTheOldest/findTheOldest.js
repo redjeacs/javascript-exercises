8cb0e7570725b76b16aa2939a593244a2ea6b367
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-18 18:19:08 +0800

    find the oldest exercise
    add a function to get the age of a person using
    date of death and birth or current date if not
    dead. reduce the array of people to just one who
    is the oldest by comparing aaccumulator to current
    person and see who's older.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+function getAge(birth, death) {
+  if(!death) {
+    death = new Date().getFullYear()
+  }
+  return death - birth;
+}
+const findTheOldest = function(array) {
+  return array.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+  })
 };
 
 // Do not edit below this line
