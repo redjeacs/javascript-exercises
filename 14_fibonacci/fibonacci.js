51ac7f18b6466c72bb565edda08d8c4d7825868a
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-18 16:46:04 +0800

    fibonacci exercise
    - check and convert argument into a number
    - return 'OOPS' for negative numbers and 0 for 0
    - run a loop to add the first and second numbers
    before the current and convert the current result
    into second previous for the next loop
    (same for the first previous)

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(numArg) {
+  let num //check and converts function argument into a number
+    if (typeof numArg !== 'number') {
+      num = parseInt(numArg)
+    }
+    else {
+      num = numArg;
+    }
+  
+  if(num < 0) {
+    return 'OOPS';
+  }
+  if(num === 0) {
+    return 0;
+  }
+  let FibonacciSecond = 1;
+  let FibonacciFirst = 0;
+  let result = 0;
+  for(let i = 2; i <= num; i++) {
+    let result = FibonacciFirst + FibonacciSecond;
+    FibonacciFirst = FibonacciSecond;
+    FibonacciSecond = result;
+  }
+  return FibonacciSecond;
 };
 
 // Do not edit below this line
