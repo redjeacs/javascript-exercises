20ac70b85211ffe7a54b2464436365c9e70be062
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-18 15:10:53 +0800

    exercise 12
    completed all calculator functions using reduce
    for arrays summation and multiplication and a for
    loop to calculate factorial bye decrementing by 1
    and multiplying it to the overall product

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+	for(let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
